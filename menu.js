fa0a06dfb700500756b23433d32209612d05e02e
David Amorim <davidfilipeamorim@gmail.com>
2022-12-28 19:00:45 +0000

    Implement add to cart functionality on menu page

--- menu.js
+++ menu.js
@@ -43,6 +43,20 @@ var vm = function () {
 
   self.shownProducts = ko.observableArray(self.products);
 
+  self.addToCart = (product) => {
+    const pid = self.products.findIndex(p => p.name === product.name);
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    if (cart.find(it => it.id === pid)) return;
+
+    cart.push({
+      id: pid,
+      qty: 1
+    });
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
   self.activate = () => {
     $("#searchBar").on("input", () => {
       self.shownProducts(self.products.filter(p => p.name.toLowerCase().startsWith($("#searchBar").val().toLowerCase())));
